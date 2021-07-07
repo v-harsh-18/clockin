8125540e0931cf95a5fa37ff99d27cc944fd5f28
Harsh <vharshverma18@gmail.com>
2021-07-07 16:19:31 +0530

    add authentication

--- server.js
+++ server.js
@@ -1,16 +1,103 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+    secret: "Our liitle secret.",
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+  googleId: String,
+  username: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3003/auth/google/clockin",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ username: profile.emails[0].value, googleId: profile.id, }, function (err, user) {
+        return cb(err, user);
+    });
+  }
+));
+
+  
 
 app.route("/")
 .get((req,res)=>{
     res.render('home');
-})
+});
+
+app.get('/auth/google',
+passport.authenticate('google', { scope: ['profile',"email"] }));
+
+app.get("/auth/google/clockin",
+  passport.authenticate('google', { failureRedirect: "/" }),
+  function(req, res) {
+    res.redirect("/calendar");
+  });
+
+  app.get("/calendar", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+      if (err){
+        console.log(err);
+      } else {
+        if (foundUsers) {
+          res.render("calendar", {usersWithSecrets: foundUsers});
+        }
+      }
+    });
+  });
+
+  
+
+
 
 app.listen(3003, () => {
     console.log('CONNECTION ESTABLISHED ON PORT 3000')
