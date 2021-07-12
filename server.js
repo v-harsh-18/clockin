62af21ef7b7f0d9b08076988a3bb24682c08f68e
Vanshika <expelliarmus912@gmail.com>
2021-07-12 17:54:13 +0530

    format

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get("/calendar", function (req, res) {
             } else {
                 if (foundUser) {
                     foundUser.toObject();
-                    console.log("heloooooooooo" + foundUser.fname)
+                    // console.log("heloooooooooo" + foundUser.fname)
                     res.render("calendar", { idpic: foundUser.picture, idname: foundUser.fname });
                 }
             }
