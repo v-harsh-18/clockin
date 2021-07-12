446d9a8db4733087b0ed6d3d794d16307603b4a2
rashimotwani <motwani.raashi@gmail.com>
2021-07-12 22:32:39 +0530

    style

--- public/JS/calendar.js
+++ public/JS/calendar.js
@@ -12,27 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
         right: 'dayGridMonth,timeGridWeek,listWeek'
       },
       dateClick: function(info) {
-        // alert('clicked ' + info.dateStr);
+        
       },
       select: function(info) {
         // alert('selected ' + info.startStr + ' to ' + info.endStr);
       },
-      // events: [
-      //   {
-      //     title  : 'event1',
-      //     start  : '2021-01-01'
-      //   },
-      //   {
-      //     title  : 'event2',
-      //     start  : '2021-01-05',
-      //     end    : '2022-01-07'
-      //   },
-      //   {
-      //     title  : 'event3',
-      //     start  : '2021-01-09T12:30:00',
-      //     allDay : false // will make the time show
-      //   }
-      // ]
+   
     });
   
     calendar.render();
