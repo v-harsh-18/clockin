586753a8ee639b7217116aff70239c84d39b4553
Noor <divnoorkaur04@gmail.com>
2021-07-28 18:11:56 +0530

    dark mode

---
+++ public/JS/sidecalendar.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   new Vue({
+//     el: '#app',
+//     data() {
+//       return {
+//         attributes: [
+//           {
+//             dot: 'red',
+//             dates: [
+//               '2021-07-22',
+//               new Date(2018, 0, 1), // Jan 1sst
+//             ],
+//           },
+          
+//         ],
+//       };
+//     }
+
+//   });
+
+// });
+
+
