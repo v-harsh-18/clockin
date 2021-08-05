75bc17f2747958efd264d2e7a9ff3afb78cd7539
rashimotwani <motwani.raashi@gmail.com>
2021-08-05 23:00:49 +0530

    bleh2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const { MongoNetworkTimeoutError } = require('mongodb');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
