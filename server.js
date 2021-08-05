3aac9926be11fadd3f668d5c10c1266105fb0d64
Vanshika Tibrewal <expelliarmus912@gmail.com>
2021-08-05 19:47:45 +0530

    deploy css

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const { MongoNetworkTimeoutError } = require('mongodb');
 const app = express();
 
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
